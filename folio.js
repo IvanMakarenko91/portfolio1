8ad22756eb109a63c451efdb8f22a7d77aed48dc
Makarenko Ivan <ivan.makarenko@free.fr>
2022-03-30 12:39:43 +0200

    Mise au propre

--- folio.js
+++ folio.js
@@ -1,3 +1,4 @@
+// Typescreept ecriture automatique.
 var app = document.getElementById('about');
 
 var typewriter = new Typewriter(app, {
@@ -9,19 +10,19 @@ typewriter
   .pauseFor(3500)
   .typeString('Mes Qualités : ')
   .pauseFor(600)
-  .typeString('<strong style="color:#620C0C">Exigeant</strong>')
+  .typeString('<strong style="color:#620C0C">Sociable</strong>')
   .pauseFor(600)
   .deleteChars(8)
   .pauseFor(600)
-  .typeString('<strong style="color:#B31010">Patient</strong>')
+  .typeString('<strong style="color:#620C0C">Engagé</strong>')
   .pauseFor(600)
-  .deleteChars(7)
+  .deleteChars(6)
   .pauseFor(600)
   .typeString('<strong style="color:#620C0C">Soucieux</strong>')
   .pauseFor(600)
   .deleteChars(8)
   .pauseFor(600)
-  .typeString('<strong style="color:#B31010">Curieux</strong>')
+  .typeString('<strong style="color:#620C0C">Curieux</strong>')
   .pauseFor(600)
   .deleteChars(7)
   .pauseFor(600)
@@ -37,37 +38,37 @@ typewriter
   .pauseFor(600)
   .deleteChars(6)
   .pauseFor(600)
-  .typeString('<strong style="color:#B31010">Disponible</strong>')
+  .typeString('<strong style="color:#620C0C">Disponible</strong>')
   .pauseFor(600)
   .deleteChars(10)
   .pauseFor(600)
-  .typeString('<strong style="color:#620C0C">Positif</strong>')
+  .typeString('<strong style="color:#620C0C">Coopératif</strong>')
   .pauseFor(2000)
-  .deleteChars(8)
+  .deleteChars(10)
   .pauseFor(600)
   .start();
 
 
-
+// Evenement hover des evaluations pour plus de details.
 const rule = document.getElementById("roll-rule");
 const dice = document.getElementById("regle-de");
 const boot = document.getElementById("boot-html");
 const evalboot = document.getElementById("eval-boot");
 
 const regle = function() {
-  rule.innerHTML = "<u>Règles du jeu</u> : Lancé le dé à 6 faces. <p>Chaque joueur lance le dé autant de fois qu'il le souhaite. Le score du dé s'accumule au même chiffre tombé dessus. Si vous tombez sur le chiffre 1, on passe le dé à l'autre joueur. Le 1er arrivé a 100 gagne.</p> "; 
+  rule.innerHTML = "<u>Règles du jeu</u> : Lancé le dé à 6 faces. <p>Chaque joueur lance le dé autant de fois qu'il le souhaite. Le score du dé s'accumule au même chiffre tombé dessus. Si vous tombez sur le chiffre 1, son score est perdu et son tour également. Le 1er arrivé a 100 gagne.</p> "; 
 }
 
 const regles = function() {
-  rule.innerHTML = "Evaluation d'entrainement Javascript. <br> Creation d'un jeu de dé consistant a atteindre <br> les 100 points en premier.</div>"; 
+  rule.innerHTML = "Évaluation d'entrainement Javascript. <br> Création d'un jeu de dé consistant à atteindre <br> les 100 points en premier.</div>"; 
 }
 
 const conception = function() {
-  boot.innerHTML = "Evaluation incluant differents elements utilisés,<br> dont la partie <u>Bootstrap</u> et sa maitrise,<br>afin de rendre le site responsive."; 
+  boot.innerHTML = "Évaluation incluant différents éléments utilisés,<br> dont la partie <u>Bootstrap</u> et sa maitrise,<br>afin de rendre le site responsive."; 
 }
 
 const conceptions = function() {
-  boot.innerHTML = "Evaluation d'entrainement HTML, CSS. Creation de 2 pages web responsive.</div>"; 
+  boot.innerHTML = "Évaluation d'entrainement HTML, CSS. Création de 2 pages web responsive.</div>"; 
 }
 
 dice.addEventListener('mouseenter',regle)
@@ -75,6 +76,7 @@ dice.addEventListener('mouseleave',regles)
 evalboot.addEventListener('mouseenter',conception)
 evalboot.addEventListener('mouseleave',conceptions)
 
+// Evenement click pour en savoir plus me concernant
 var content = document.getElementById("content");
 var button = document.getElementById("show-more");
 
